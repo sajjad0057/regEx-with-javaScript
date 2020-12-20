9332bf8df9e710e7eaf52638719a1fae0fccc983
sajjad Hossain <sajjadhossain0057@gamil.com>
2020-12-21 04:34:54 +0600

    Literal and Meta Characters

--- script.js
+++ script.js
@@ -4,75 +4,41 @@
 
 let re;
 let str;
-re = /hello/i;   // i = caseInsensitive              
 
-// /'regex'/   .... starting "/"   and Ending "/"  generally contain regular Expression
+//literal Character 
+re = /hello/   // casesensitive
+re = /hello/i;  // caseInsensitive
+re = /lo W/i;
 
-// console.log(re);       //   result=>   /hello/
-// console.log(re.source);   // result =>  hello
-
-
-str = 'Hello world';
-str = "again Hello world"
-str = "fdshellodjfj world"
-str = "again Hello world hello"
-
-
-/*
-exec() func --> returns result in an array or null.If matching, this array contain matchig value , index and full str,
-The exec() method tests for a match in a string.
-This method returns the matched text if it finds a match, otherwise it returns null.
-*/
-
-
-let result = re.exec(str)
-console.log("exec() return:",result);
-
-
-/*
-The test() method tests for a match in a string.
-This method returns true if it finds a match, otherwise it returns false.
-*/ 
-
-result = re.test(str)
-console.log("test() retuen:",result);
+// Meta Characters :
+re = /^hello/i;   //Must start with
+re = /hello$/i;   // Must End With
+re = /^hello$/;   // Must start and End with
+re = /^he.llo$/;      //  match any one character the with "dot". position
+re = /h*llo/;       // Zero or more  time  character match with "h" in  * position 
+re = /he?llo/;    // by using ? means "e" optional here.
+re = /he?a?llo/;   // by using ? means "e" and "a" optional here.
+re = /hello\?/;   // by escaping use "/"  before   "?" , here "?"  has been as a part of pattern.
 
 
 
-/*
-match() same as exec() function, difference between their Syntax.
-The match() method searches a string for a match against a regular expression,
-and returns the matches, as an Array object.
-Syntax:
-string.match(regexp)
-*/
-
-result = str.match(re)
-console.log("match() return:",result);
 
 
 
-/*
-The search() method searches a string for a specified value, and returns the position of the match.
-The search value can be string or a regular expression.
-This method returns -1 if no match is found.
-Syntax:
-string.search(searchvalue)
+str = "he llo"
+str = "hexllo"
+str = "hello"
+str = "hello?"
 
-*/
+console.log(re.exec(str));
 
-result = str.search(re)
-console.log("search() return :", result);
+function reTest(re,str){
+    if(re.test(str)){
+        console.log(`'${str}' matches '${re.source}'`);
+    }
+    else{
+        console.log(`'${str}' dosen't matches '${re.source}'`);
+    }
+}
 
-
-/*
-The replace() method searches a string for a specified value,
-or a regular expression, and returns a new string where the specified values are replaced.
-Syntax:
-string.replace(searchvalue, newvalue)
-*/
-
-result = str.replace(re,"hi")
-console.log("str:",str);
-console.log("replace() return:",result);
-
+reTest(re,str)
