8b5dc253375d3e0ff4f80166f0dfc6eb1ff37e32
Nafiul <nafiul.ice.pust@gmail.com>
2020-12-21 19:15:34 +0600

    Added frontend

--- script.js
+++ script.js
@@ -1,57 +1,37 @@
-// Regular Expression :
+const input_string = document.getElementById("istring");
+const regex_string = document.getElementById("rstring");
+const parentdiv = document.getElementById("parent");
 
-// Pattern Matching Technique :
+regex_string.addEventListener("input", (event) => {
+  const rvalue = event.target.value;
+  const ivalue = input_string.value;
+  update(ivalue, rvalue);
+});
 
-let re;
-let str;
-
-
-// Check Postal Code :
-
-re = /^[0-9]{4}$/
-
-str = "4040"
-
-// Check Phone Number :
-
-re = /^01[0-9]{9}$/
-re = /^(\+88)?01[0-9]{9}$/
-re = /^(\+)?(88)?01[0-9]{9}$/
-
-
-str = "01774354369"
-str = "8801774354369"
-str = "+8801774354369"
-
-
-
-// Check Email address :
-//sajjadhossain0057@gmail.com
-
-re = /^([a-zA-Z0-9])+$/
-re = /^([a-zA-Z0-9]+\.?_?)+[^\.]@$/
-re = /^([a-zA-Z0-9]+\.?_?)+[^\.]@([a-zA-Z0-9]+\.?_?)+[^\.]$/
-
-str="sajjadhossain0057"
-str="sajjadhossain0057._"
-str="sajjadhossain0057._fdg."
-str="sajjadhossain0057._fdg@"
-str = "sajjadhossain0057@gmail.com"
-
-
-
-
-
-
-console.log(re.exec(str));
-
-function reTest(re,str){
-    if(re.test(str)){
-        console.log(`'${str}' matches '${re.source}'`);
-    }
-    else{
-        console.log(`'${str}' dosen't matches '${re.source}'`);
-    }
+input_string.addEventListener("input", (event) => {
+  const ivalue = event.target.value;
+  const rvalue = regex_string.value;
+  update(ivalue, rvalue);
+});
+function update(i, r) {
+  let htmlstring = ``;
+  const re = RegExp(r, "g");
+  const values = re.exec(i);
+  for (let value in values) {
+    htmlstring += `<div class="level">
+    <div class="level-left">
+        <div class="level-item">
+            Key: ${value}
+        </div>
+    </div>
+    <div class="level-right">
+        <div class="level-item has-text-centered">
+            ${values[value]}
+        </div>
+    </div>
+</div>`;
+  }
+  parentdiv.innerHTML = htmlstring;
 }
 
-reTest(re,str)
+regex_string.dispatchEvent(new Event("input"));
